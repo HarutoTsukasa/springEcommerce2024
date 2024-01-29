08d3f419dba0ab441357ac91ac186ca8e4e49981
nejarino <njriano2@misena.edu.co>
2024-01-29 10:51:37 -0500

    creacionEstucturaProyectoEcommerce&clasesModel

---
+++ src/main/java/com/sena/ecommerce/SpringEcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.sena.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//*Decirle al metodo main que es el que levanta la conexion que de momento no haga la conexcion a la DB*//
+public class SpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceApplication.class, args);
+	}
+
+}
