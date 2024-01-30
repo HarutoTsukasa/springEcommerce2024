2cad497e4df61b0e644394c35db73baf78e588d7
nejarino <njriano2@misena.edu.co>
2024-01-30 11:04:27 -0500

    v0.6_funcionalidadParaEliminarUnRegistroEnProducto_30012024

--- src/main/java/com/sena/ecommerce/controller/ProductoController.java
+++ src/main/java/com/sena/ecommerce/controller/ProductoController.java
@@ -79,4 +79,11 @@ public class ProductoController {
 		return "redirect:/productos";
 	}
 
+	// metodo para eliminar registro de productos
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productoService.delete(id);
+		return "redirect:/productos";
+	}
+
 }
